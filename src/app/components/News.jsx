38a72d97e1bb76d0f85417d96d0d08d5b97505fb
jeannot diambu <diambujeannot@gmail.com>
2024-01-25 12:04:01 +0100

    create section news

---
+++ src/app/components/News.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { news } from "../helper/news";
+import Image from "next/image";
+
+function News() {
+  return (
+    <div className="bg-[#eeeff3] px-16 py-8">
+      <h2 className="text-center py-7 text-[27px] font-bold">Nouvelles</h2>
+      <div className="flex gap-10 ">
+        {news.map((news) => (
+          <div
+            key={news.id}
+            className="bg-white px-7 py-5 cursor-pointer rounded-[8px] boxshadow hover:boxshadow-hover hover:bg-[rgba(255,255,255,0.7)]"
+          >
+            <div>
+              <div className="text-[15px] text-end text-[rgba(0,0,0,0.82)]">
+                {news.date}
+              </div>
+              <div className="py-4">
+                <Image src={news.image} alt="" className="rounded-[8px] " />
+              </div>
+              <h3 className="font-bold text-[22px] pb-3 underline text-[rgba(0,0,0,0.76)]">
+                {news.title}
+              </h3>
+              <div className="text-[15px] font-light">{news.description}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default News;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,16 @@
 import Image from "next/image";
-import Header from "./components/Header"
+import Header from "./components/Header";
+import News from "./components/News";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <Header />
-    </main>
+    <div>
+      <main className=" flex min-h-screen flex-col items-center justify-between">
+        <Header />
+      </main>
+      <div>
+        <News />
+      </div>
+    </div>
   );
 }
