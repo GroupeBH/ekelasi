4beba7765746493053bdafc970c952b396f1d791
jeannot diambu <diambujeannot@gmail.com>
2024-01-25 17:36:45 +0100

    update home

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -9,16 +9,16 @@ export default function Header() {
           <div className="flex justify-between items-center w-[100%]">
             <div className="md:w-[50%]">
                 <div className="w-[100%]">
-                    <p className="md:text-4xl text-2xl font-medium pb-3">Plateforme éducative numérique ekelasi</p>
-                    <p className="text-sm">Traduction et accompagnement dans l'environnement numérique des processus des organisations éducatives et des instances dirigeantes</p>
+                    <p className="md:text-[48px] md:pb-5 font-bold leading-[60px] text-2xl pb-3">Plateforme éducative <br /> numérique <br /> <span className="text-[#3972b7]">e-ke</span><span className="text-[#ce1322]">la</span><span className="text-[#f7d715]">si</span></p>
+                    <p className="text-sm md:text-[16px] md:pb-5 text-[rgba(0,0,0,0.7)] font-light">{"Traduction et accompagnement dans l'environnement numérique des processus des organisations éducatives et des instances dirigeantes"}</p>
                 </div>
                 <div className="flex gap-3 text-lg font-medium mt-5">
-                    <button className="px-5 md:py-3 py-3 bg-red-600 rounded-lg text-white">Entrer</button>
-                    <button className="md:px-5 md:py-3 py-3 px-1 bg-blue-600 rounded-lg text-white">Connecter l'école</button>
+                    <button className="px-5 md:py-[6px] md:text-[15px] py-3 bg-[#ce1322] rounded-lg text-white">Entrer</button>
+                    <button className="md:px-5 md:py-[6px] md:text-[15px] py-3 px-1 bg-[#3972b7] rounded-lg text-white">{"Connecter l'école"}</button>
                 </div>
             </div>
             <div className="md:block hidden">
-                <Image src={side_image} className="h-[65vh] w-[40vw]" />
+                <Image src={side_image} alt="" className="h-[65vh] w-[40vw]" />
             </div>
           </div>
         </div>
