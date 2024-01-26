6ad7fd1123778496a798e002846dc6069adeb475
eugene <bosukuege@gmail.com>
2024-01-26 14:22:49 +0100

    add icon in metadata

--- src/app/components/Partners.js
+++ src/app/components/Partners.js
@@ -8,13 +8,12 @@ export default function Partners() {
         <div className="bg-[#eeeff3] md:p-16 p-5">
             <h3 className="md:text-[27px] text-center font-bold text-[25px]">Ils travaillent avec nous</h3>
             <div className='flex justify-center items-center gap-10 pt-10'>
-                {partnersImgs.map((i, index) => {
-                    return(
-                      <div key={index}>
-                        <Image alt='' src={i} key={index} className="h-[20vh] w-[30vw]" />
-                      </div>
-                    )
-                })}
+                <div>
+                    <Image alt='' src={numGouv} className="h-[20vh] w-[22.5vw]" />
+                </div>
+                <div>
+                    <Image alt='' src={epstGouv} className="h-[20vh] w-[37.5vw]" />
+                </div>
             </div>
         </div>
     )
