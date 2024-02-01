7120ba591de047607a8ef67182e1cc18e4ed1a55
jeannot diambu <diambujeannot@gmail.com>
2024-02-01 16:18:09 +0100

    add date in news

--- src/app/components/news/News.js
+++ src/app/components/news/News.js
@@ -5,6 +5,8 @@ import Image from 'next/image'
 import Nav from '../Nav'
 import { useRouter } from 'next/navigation'
 import HeaderAdmin from '../header/HeaderAdmin'
+import Moment from 'react-moment';
+import 'moment-timezone';
 
 function AllNews() {
 
@@ -40,16 +42,19 @@ function AllNews() {
                     className="bg-white px-7 h-auto py-5 cursor-pointer rounded-[8px] shadow-[0_0_0_rgba(48, 48, 49, 0.3)] hover:shadow-[0_0_15px_rgba(20,_20,_20,_0.07)] transition-all ease-in-out delay-150 hover:bg-[rgba(255,255,255,0.7)]"
                 >
                     <div>
-                    <div className="text-[15px] text-end text-[rgba(0,0,0,0.82)]">
-                        {news.date}
-                    </div>
-                    <div className="py-4">
-                        <Image src={news?.image} width={100} height={100} alt="" className="w-[100%] h-[250px] object-cover rounded-[8px] " />
-                    </div>
-                    <h3 id="news" className="font-bold text-[17px] pb-3 underline text-[rgba(0,0,0,0.76)]">
-                        {news.title}
-                    </h3>
-                    <div  className="text-[15px] font-light max-w-[300px] overflow-hidden whitespace-nowrap text-ellipsis">{news.description}</div>
+                        <div className="text-[15px] text-end text-[rgba(0,0,0,0.82)]">
+                            {news.date}
+                        </div>
+                        <div className="py-4">
+                            <Image src={news?.image} width={100} height={100} alt="" className="w-[100%] h-[250px] object-cover rounded-[8px] " />
+                        </div>
+                        <h3 id="news" className="font-bold text-[17px] pb-3 underline text-[rgba(0,0,0,0.76)]">
+                            {news.title}
+                        </h3>
+                        <div  className="text-[15px] font-light max-w-[300px] overflow-hidden whitespace-nowrap text-ellipsis">{news.description}</div>
+                        <div className='text-end font-medium italic pt-[25px]'>
+                            <Moment date={news?.createdAt} format="DD-MM-YYYY HH:mm" />
+                        </div>
                     </div>
                 </div>
                 ))}
