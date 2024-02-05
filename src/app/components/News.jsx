948f4a180c88d76f2d40b74570e6dfbbb244c158
jeannot diambu <diambujeannot@gmail.com>
2024-02-05 12:13:09 +0100

    display news in hom

--- src/app/components/News.jsx
+++ src/app/components/News.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+'use client';
+import React, { useState } from "react";
 import { news } from "../helper/news";
 import Image from "next/image";
+import axios from "axios"
 import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
 import { useEffect } from "react";
@@ -9,6 +11,19 @@ import Link from "next/link";
 
 function News() {
 
+  const [newss, setNewss] = useState([])
+
+
+  const getNews = async() => {
+    const data = await axios.get("http://localhost:8002/api/get-news/")
+    console.log("news : ", data.data.news.reverse().slice(0, 3))
+    setNewss(data.data.news.slice(0, 3))
+  }
+
+  useEffect(() => {
+    getNews()
+  },[])
+
   useEffect(() => {
     AOS.init();
   }, []);
@@ -16,10 +31,10 @@ function News() {
   return (
     <div className="px-[20px] bg-[#eeeff3] md:px-16 py-8">
       <h2 className="text-center py-7 text-[25px] md:text-[27px] font-bold" >Nouvelles</h2>
-      <div className=" flex flex-col md:flex-row gap-10" data-aos="fade-up" data-aos-delay="100">
-        {news.map((news) => (
+      <div className=" grid grid-cols-1 md:grid-cols-3 gap-10" data-aos="fade-up" data-aos-delay="100">
+        {newss.map((news) => (
           <div
-            key={news.id}
+            key={news._id}
             className="bg-white px-7 py-5 cursor-pointer rounded-[8px] shadow-[0_0_0_rgba(48, 48, 49, 0.3)] hover:shadow-[0_0_15px_rgba(20,_20,_20,_0.07)] transition-all ease-in-out delay-150 hover:bg-[rgba(255,255,255,0.7)]"
           >
             <div>
@@ -27,12 +42,12 @@ function News() {
                 {news.date}
               </div>
               <div className="py-4">
-                <Image src={news.image} alt="" className="rounded-[8px] " />
+                <Image src={news.image} width={20} height={20} alt="" className=" w-[100%] h-[200px] object-cover rounded-[8px] " />
               </div>
-              <h3 id="news" className="font-bold text-[22px] pb-3 underline text-[rgba(0,0,0,0.76)]">
+              <h3 id="news" className="font-bold text-[20px] pb-3 underline text-[rgba(0,0,0,0.76)]">
                 {news.title}
               </h3>
-              <div  className="text-[15px] font-light">{news.description}</div>
+              <div  className="text-[15px] font-light max-w-[300px] overflow-hidden whitespace-nowrap text-ellipsis">{news.description}</div>
             </div>
           </div>
         ))}
